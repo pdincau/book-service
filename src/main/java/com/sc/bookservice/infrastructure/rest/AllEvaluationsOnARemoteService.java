9502047c944e734b891bc8bc54a58c384b2593a3
Paolo D'Incau <paolo.dincau@gmail.com>
2018-09-08 23:53:19 +0200

    Added hystrix

--- src/main/java/com/sc/bookservice/infrastructure/rest/AllEvaluationsOnARemoteService.java
+++ src/main/java/com/sc/bookservice/infrastructure/rest/AllEvaluationsOnARemoteService.java
@@ -1,5 +1,6 @@
 package com.sc.bookservice.infrastructure.rest;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sc.bookservice.domain.AllEvaluations;
 import com.sc.bookservice.domain.Evaluation;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 
 public class AllEvaluationsOnARemoteService implements AllEvaluations {
 
@@ -20,8 +22,14 @@ public class AllEvaluationsOnARemoteService implements AllEvaluations {
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "defaultEvaluations")
     public List<Evaluation> forBookWithIsbn(String isbn) {
         ResponseEntity<Evaluation[]> response = template.getForEntity("http://{evaluationServiceId}/evaluations/{isbn}", Evaluation[].class, evaluationServiceId, isbn);
         return asList(response.getBody());
+    }
+
+    @SuppressWarnings("unused")
+    List<Evaluation> defaultEvaluations(String isbn) {
+        return emptyList();
     }
 }
