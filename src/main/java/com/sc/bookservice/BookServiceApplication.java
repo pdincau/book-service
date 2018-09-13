ca5dbd8d0e0aa010c7760f8fcf840fe3416c8284
Paolo D'Incau <paolo.dincau@gmail.com>
2018-09-13 11:33:18 +0200

    Exposed hystrix dashboard

--- src/main/java/com/sc/bookservice/BookServiceApplication.java
+++ src/main/java/com/sc/bookservice/BookServiceApplication.java
@@ -3,9 +3,11 @@ package com.sc.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
